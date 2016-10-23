7fc1ada744ebd6cf0f1a61085b7c96f596356e59
Max Pleaner <maxpleaner@gmail.com>
2016-10-23 11:36:11 -0700

    Update effects.rb

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,7 +1,7 @@
 module Sounds::Effects
   def stagger(sound, opts)
     eighth_note_rest_time = 60.0 / $tempo_bpm
-    beat_type = opts[:beat_type] || 64
+    beat_type = opts[:beat_type] || 64.0
     custom_rest_time = eighth_note_rest_time / (beat_type / 8.0)
     (opts[:beats] || 4).times do
       play sound
